cb2c88d2eca1f5b2ad61f4282aaf1e28b8b4ed75
Aryan Bhalla <aryanbhalla@Aryans-MacBook-Air.local>
2026-03-27 00:55:53 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-red-600 text-6xl font-bold tracking-widest">
+        Aryan Bhalla
+      </h1>
+    </div>
+  );
+}
