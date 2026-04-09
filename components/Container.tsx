3ecf7508fac9ce859cf1277991d31df28cb138a1
Aryan Bhalla <aryanbhalla@Aryans-MacBook-Air.local>
2026-04-10 02:40:04 +0530

    typescript error resovled

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,4 +1,8 @@
-const Container = ({ children }) => {
+type Props = {
+  children: React.ReactNode;
+};
+
+const Container = ({ children }: Props) => {
   return (
     <div className="w-full flex justify-center bg-black text-white">
       <div className="w-full max-w-4xl border-l border-r border-neutral-800">
